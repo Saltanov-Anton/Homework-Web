2ae6f9a32a28723443942f2a74ff1bfee6aad472
Anton Saltanov <saltanov.anton@yandex.ru>
2023-02-06 11:31:40 +0900

    commit 2

---
+++ src/main/java/com/example/homworkwebapp/HomworkWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.homworkwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomworkWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomworkWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homworkwebapp/HomworkWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homworkwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomworkWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
